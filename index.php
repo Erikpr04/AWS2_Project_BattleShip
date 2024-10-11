9b7e536e81a845345a3ad753f5f6c9bfd5c6c910
josehtat <jhurtadot33@gmail.com>
2024-10-11 19:58:43 +0200

    punto 6 acabado

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Shortline Strike</title>
+</head>
+
+<body>
+    <div class="background">
+        <div class="container">
+            <div class="titleIndex">
+                <h1>Shortline Strike</h1>
+            </div>
+            <div class="descriptionIndex">
+
+            </div>
+            <div class="options">
+                <a href="game.php"><button id="buttonPlay">PLAY</button></a>
+                <br>
+                <a href="ranking.php"><button id="buttonRanking">HALF OF TIME</button></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
