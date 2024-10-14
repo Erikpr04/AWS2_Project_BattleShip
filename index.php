fbffb7ca709b62f86c21ca808d5ee3252d77d405
josehtat <150950561+josehtat@users.noreply.github.com>
2024-10-14 11:05:12 +0200

    actualizacion index y style

--- index.php
+++ index.php
@@ -1,29 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Shortline Strike</title>
 </head>
-
 <body>
-    <div class="background">
-        <div class="container">
+    <div class="backgroundIndex">
+        <div class="containerIndex">
             <div class="titleIndex">
                 <h1>Shortline Strike</h1>
             </div>
             <div class="descriptionIndex">
-
             </div>
-            <div class="options">
-                <a href="game.php"><button id="buttonPlay">PLAY</button></a>
+            <div class="optionsIndex">
+                <button id="buttonPlayIndex" onclick="location.href='game.php'">PLAY</button>
                 <br>
-                <a href="ranking.php"><button id="buttonRanking">HALF OF TIME</button></a>
+                <button id="buttonRankingIndex" onclick="location.href='ranking.php'">HALF OF TIME</button>
             </div>
         </div>
     </div>
 </body>
-
 </html>
