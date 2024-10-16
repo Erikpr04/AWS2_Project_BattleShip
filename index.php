ef70e5d207216b294ec4848823b036bde6565832
josehtat <jhurtadot33@gmail.com>
2024-10-16 19:31:43 +0200

    .

--- index.php
+++ index.php
@@ -12,24 +12,27 @@
     <div class="descriptionIndex">
         <h2>Descripción del Juego</h2>
         <p>
-            "Hundir la Flota" es un juego clásico de estrategia naval, en el cual dos jugadores compiten para hundir los barcos del oponente. Cada jugador coloca sus barcos en un tablero cuadrado y, por turnos, intenta adivinar las posiciones de los barcos del otro.
+            "Hundir la Flota" es un juego clásico de estrategia naval, en el cual dos jugadores compiten para hundir los barcos del oponente.
+            Cada jugador coloca sus barcos en un tablero cuadrado y, por turnos, intenta adivinar las posiciones de los barcos del otro.
         </p>
         <p>
-            El tablero tiene un tamaño típico de 10x10, donde las filas están etiquetadas con letras y las columnas con números. Los barcos se colocan horizontal o verticalmente, y varían en tamaño. Los tipos de barcos pueden incluir:
+            El tablero tiene un tamaño típico de 10x10, donde las filas están etiquetadas con letras y las columnas con números.
+            Los peces se colocan horizontal o verticalmente, y varían en tamaño. Los tipos de pueden pueden incluir:
         </p>
         <ul>
-            <li>Acorazado (4 casillas)</li>
-            <li>Crucero (3 casillas)</li>
-            <li>Submarino (3 casillas)</li>
-            <li>Destructor (2 casillas)</li>
+            <li>Anguila (5 casillas)</li>
+            <li>Pez espada (4 casillas)</li>
+            <li>Calamar (3 casillas)</li>
+            <li>Pez (2 casillas)</li>
         </ul>
         <p>
-            Los jugadores deben marcar cada disparo en el tablero. Si aciertan en la ubicación de un barco, se anota como un "impacto"; si fallan, es "agua". El objetivo del juego es hundir todos los barcos del oponente antes de que hundan los tuyos.
+            Los jugadores deben marcar cada disparo en el tablero.
+            Si aciertan en la ubicación de un pez, se anota como un "tocado"; si fallan, es "agua".
+            El objetivo del juego es cazar todos los peces del oponente antes de que cacen los tuyos.
         </p>
         <p>
             Este juego ha sido una popular actividad de entretenimiento desde su invención, proporcionando diversión y desarrollando habilidades de pensamiento estratégico y lógico.
         </p>
-        
     </div>
     </div>
     <div class="backgroundIndex">
@@ -39,7 +42,7 @@
             </div>
             <div class="optionsIndex">
                 <noscript>
-                <button id="buttonPlayIndex" disabled>PLAY</button>
+                    <button id="buttonPlayIndex" disabled>PLAY</button>
                 </noscript>
                 <button id="buttonPlayIndex"><a href="game.php">PLAY</a></button>
                 <br>
