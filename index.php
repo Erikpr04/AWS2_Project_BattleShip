baa04c5d9d26d2ab3bf121a2ac5d5d95cf2529a2
Mar <mar.melich@gmail.com>
2024-10-17 11:56:56 +0200

    arreglado idioma y caracteres username

--- index.php
+++ index.php
@@ -11,28 +11,28 @@
 <body class="bodyIndex">    
     <main class="mainContent">
         <section class="descriptionIndex">
-            <h2>Descripción del Juego</h2>
+            <h2>Descripció del Joc</h2>
             <p>
-                "Hundir la Flota" es un juego clásico de estrategia naval, en el cual dos jugadores compiten para hundir los barcos del oponente.
-                Cada jugador coloca sus barcos en un tablero cuadrado y, por turnos, intenta adivinar las posiciones de los barcos del otro.
+                Enfonsar la Flota és un joc clàssic d'estratègia naval, en el qual dos jugadors competeixen per enfonsar els vaixells de l'oponent.
+                Cada jugador col·loca els vaixells en un tauler quadrat i, per torns, intenta endevinar les posicions dels vaixells de l'altre.
+                Soreline Strike però, és una versió del clàssic ambientada en un dia de platja i pesca, on els vaixells es presenten com a peixos.
             </p>
             <p>
-                El tablero tiene un tamaño típico de 10x10, donde las filas están etiquetadas con letras y las columnas con números.
-                Los peces se colocan horizontal o verticalmente, y varían en tamaño. Los tipos de pueden incluir:
-            </p>
+                El tauler té una mida típica de 10x10, on les files estan etiquetades amb números i les columnes amb lletres.
+                Els peixos es col·loquen horitzontalment o verticalment, i varien en mida. Els tipus de poden incloure:</p>
             <ul>
-                <li>Anguila (5 casillas)</li>
-                <li>Pez espada (4 casillas)</li>
-                <li>Calamar (3 casillas)</li>
-                <li>Pez (2 casillas)</li>
+                <li>Anguila (5 caselles)</li>
+                <li>Peix espasa (4 caselles)</li>
+                <li>Calamar (3 caselles)</li>
+                <li>Peix (2 caselles)</li>
             </ul>
             <p>
-                Los jugadores deben marcar cada disparo en el tablero.
-                Si aciertan en la ubicación de un pez, se anota como un "tocado"; si fallan, es "agua".
-                El objetivo del juego es cazar todos los peces del oponente antes de que cacen los tuyos.
+                Els jugadors han de marcar cada tret al tauler.
+                Si l'encerten a la ubicació d'un peix, s'anota com un "tocat"; si fallen, és “aigua”.
+                Lobjectiu del joc és caçar tots els peixos de l'oponent abans que cacin els teus.
             </p>
             <p>
-                Este juego ha sido una popular actividad de entretenimiento desde su invención, proporcionando diversión y desarrollando habilidades de pensamiento estratégico y lógico.
+                Aquest joc ha estat una activitat d'entreteniment popular des de la seva invenció, proporcionant diversió i desenvolupant habilitats de pensament estratègic i lògic. 
             </p>
         </section>
 
