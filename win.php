2df6b11fb252e63b7beee038912b48593de7be34
Mar <mar.melich@gmail.com>
2024-10-24 13:39:15 +0200

    ia millorada

--- win.php
+++ win.php
@@ -62,7 +62,7 @@ if (
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>You Won!</title>
+    <title>Has guanyat!</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
