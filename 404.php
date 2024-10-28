d95fed82f3f56efaf2b7710cd4be85e14369f7c5
josehtat <jhurtadot33@gmail.com>
2024-10-28 17:49:38 +0000

    archivo error 404 creado

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pàgina No Trobat</title>
+</head>
+<body>
+    <h1>404 - Pàgina No Trobat</h1>
+    <p>Ho sentim, però la pàgina que estàs buscant no existeix.</p>
+</body>
+</html>
